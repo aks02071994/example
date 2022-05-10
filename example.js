16681ecf2d19e7aefbaef07750c1912a9a5d47fe
Ankit Singh <ankit.as.singh1994@gmail.com>
2022-05-11 01:38:50 +0530

    new line

---
+++ example.js
@@ -0,0 +1,13 @@
+
+function input(a,b) {
+ 
+    if (a===b) {
+        console.log(true);    
+        
+    }
+    else{
+        console.log(false);
+
+    }
+ }
+input(5, "5")
---
+++ project1.js
@@ -0,0 +1,179 @@
+// firstcontainer.innerHTML= "This is a new line";
+
+
+// document.getElementsByTagName('h1')[0].innerHTML= "This is a Good Things";
+// document.getElementsByTagName('h1')[0].style.color= "blue";
+// document.getElementsByClassName('container')[0].style.color= "red";
+// document.getElementsByClassName('container')[1].style.color= "green";
+// console.log("Hello World")
+
+
+
+// let number= 10;
+// function good_as(num) {
+         
+//     for(let i=1; i<=num; i++ )
+//     if (i %2 === 0) {
+//         console.log(i + "="+"even")
+//     }
+//     else if() {
+//         console.log(i + "="+"odd")
+  
+//      }
+
+//     }
+
+//      good_as(number)
+
+
+
+//  function myFunction(a, b)
+// {
+ 
+//  console.log(a+b)
+// }
+// myFunction(100,200)
+
+// function input(a,b) {
+ 
+//     if (a===b) {
+//         console.log(true);    
+        
+//     }
+//     else{
+//         console.log(false);
+
+//     }
+//  }
+// input(5, "5")
+
+
+
+// let a= 10;
+// console.log(typeof a)
+
+
+
+// function output(a){
+//     console.log(typeof a)
+// }
+// output(true)
+
+
+
+// let a= "abcd";
+// console.log(a[1])
+
+
+
+// function myfunction(a, n) {
+//     console.log(a[n])
+    
+// }
+// myfunction("abcd", 1)
+
+
+
+// const a =10, b = 5, c = "Hello World";
+// console.log(a == 10);
+// console.log(b == 5);
+// console.log(c == 'Hello world');
+
+// output: true true false
+// == / comparison operater
+// =  / assignment operater
+
+
+
+// const a = 10, b = 'hello world'
+// console.log(a != 10);  //false
+// console.log(a != 230);  //true
+// console.log(b != 'hello world');  //false
+// console.log(b != 10);   //true
+
+
+
+// let n = 5
+// for (let i=0; i<n; i++){
+//     console.log("Hello World");
+// }
+
+// output: 5 times 'Hello World'
+
+
+
+// let num= 5
+// for (let i=1; i<=num; i++){
+//     console.log(i)
+// }
+
+//output: 1 2 3 4 5
+
+
+
+// let sum=0
+// const  n=10
+// for (let i=1; i<=n; i++){
+//     sum += i;
+
+// }
+// console.log(sum)
+
+// output: 55
+
+
+
+// let sum= 0
+//    num= 10
+//    for(let i = 2; i<=100; i=i+2){
+//        sum += i;
+//    }
+//    console.log(sum)
+
+// output: 30
+
+
+
+// let sum= 0
+//    num= 10
+   
+//    for(let i = 1; i<=num; i=i+2){
+//        sum += i  ;   //sum = sum + i
+//    }
+//    console.log(sum)
+
+//    output: 25
+
+
+
+// for(let i = 1; i<=10; i++){
+
+//     if (i%2===0) {
+//         console.log(i, "Ankit")
+//     }
+//     else{
+//         console.log(i, "Pratik")
+
+//     }
+// 
+
+// for (let i = 1; i <=20; i++) {
+//     if (i%6===0) {
+
+//     console.log(i,"Pratik"+"Ankit");
+//     }
+
+//     else if (i%2===0) {
+//         console.log(i, "Ankit")
+//     }
+//     else if(i%3===0){
+//         console.log(i, "Pratik")
+
+//     }
+    
+// }
+
+
+
+let str="Get last n characters of string"
+let arr=[];
